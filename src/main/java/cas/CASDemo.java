f07f1c2d2103523a7405b7e9ef5d1da2290b60a1
18811 <18811392166@163.com>
2024-01-09 23:31:42 +0800

    7

--- src/main/java/cas/CASDemo.java
+++ src/main/java/cas/CASDemo.java
@@ -1,8 +1,18 @@
 package cas;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * CAS比较并交换 compareAndSet
+ */
 public class CASDemo {
 
     public static void main(String[] args) {
-
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+        //main do thing.....
+        System.out.println(atomicInteger.compareAndSet(5, 2024) +", current data: "+atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(5, 2024) +", current data: "+atomicInteger.get());
+        //TODO 原理: CAS的执行靠的是底层的unsafe类来保证原子性，Unsafe类大多是 rt.jar原生的，Unsafe包里面的类，native修饰，这是底层额类，这是一个后门，C指针直接曹总内存，
+        // 是系统原语，很多的指令组成，不允许打断，CPU的原子指令，不会造成数据不一致的问题
     }
 }
