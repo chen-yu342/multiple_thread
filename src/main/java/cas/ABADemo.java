a78161f52aefbf411ec0be8995b9d3e33df2740b
18811 <18811392166@163.com>
2024-01-10 22:56:41 +0800

    ABA

---
+++ src/main/java/cas/ABADemo.java
@@ -0,0 +1,53 @@
+package cas;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+class User{
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    String  username;
+    int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+public class ABADemo {
+    public static void main(String[] args) {
+        //TODO 这里是原子引用
+        User zs = new User("zs", 23);
+        User li4 = new User("li4", 29);
+
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+        atomicReference.set(zs);
+        System.out.println(atomicReference.compareAndSet(zs,li4)+"\t"+atomicReference.get());
+        System.out.println(atomicReference.compareAndSet(zs,li4)+"\t"+atomicReference.get());
+        //TODO true	User{username='li4', age=29}
+        //    false	User{username='li4', age=29}
+        //TODO 解决ABA问题 ： 新增一种机制，那就是修改版本号（类似时间戳） 不仅是比较值，还要比较版本号 JUC atomicStampedReference class
+
+    }
+}
--- src/main/java/cas/CASDemo.java
+++ src/main/java/cas/CASDemo.java
@@ -12,7 +12,10 @@ public class CASDemo {
         //main do thing.....
         System.out.println(atomicInteger.compareAndSet(5, 2024) +", current data: "+atomicInteger.get());
         System.out.println(atomicInteger.compareAndSet(5, 2024) +", current data: "+atomicInteger.get());
-        //TODO 原理: CAS的执行靠的是底层的unsafe类来保证原子性，Unsafe类大多是 rt.jar原生的，Unsafe包里面的类，native修饰，这是底层额类，这是一个后门，C指针直接曹总内存，
+        //TODO 原理: CAS的执行靠的是底层的unsafe类来保证原子性，Unsafe类大多是 rt.jar里面的，原生的，Unsafe包里面的类，native修饰，
+        // 这是底层额类，这是一个后门，C指针直接曹总内存，
         // 是系统原语，很多的指令组成，不允许打断，CPU的原子指令，不会造成数据不一致的问题
+        // CAS 的缺点，最大的问题就是ABA问题？如何规避ABA问题呢？ABA:狸猫换太子，A、B两个线程，A需要10s，B 2s完成....B把value=1改为2，
+        // 最后后又改为了1，此时A进来，一看值是原来期待的1，但是其实不是了
     }
 }
